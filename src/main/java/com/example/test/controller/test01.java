18b99b3f6c29a5b140949786746c63b7fc01eee0
rotsge <ld7321@163.com>
2022-09-14 21:31:52 +0800

    本地测试

--- src/main/java/com/example/test/controller/test01.java
+++ src/main/java/com/example/test/controller/test01.java
@@ -1,10 +1,13 @@
 package com.example.test.controller;
 
+
 import org.springframework.stereotype.Controller;
 
-import javax.annotation.Resource;
+import java.sql.SQLOutput;
 
 @Controller
 public class test01 {
-
+        public static void main(){
+            System.out.println("本地");
+        }
 }
